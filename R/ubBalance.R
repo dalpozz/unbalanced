56beeadfbed6219d90bfae3d57eb0f7302ab317e
dalpozz <dalpozz@gmail.com>
2017-09-04 15:57:22 +0200

    doc ubBalance

--- R/ubBalance.R
+++ R/ubBalance.R
@@ -1,3 +1,46 @@
+#' Balance wrapper
+#'
+#' The function implements several techniques to re-balance or remove noisy instances in unbalanced datasets.
+#'
+#' @param X the input variables of the unbalanced dataset.
+#' @param Y the response variable of the unbalanced dataset.
+#' @param type the balancing technique to use (ubOver, ubUnder, ubSMOTE, ubOSS, ubCNN, ubENN, ubNCL, ubTomek).
+#' @param positive the majority class of the response variable.
+#' @param percOver parameter used in ubSMOTE
+#' @param percUnder parameter used in ubSMOTE
+#' @param k parameter used in ubOver, ubSMOTE, ubCNN, ubENN, ubNCL}
+#' @param perc parameter used in ubUnder
+#' @param method parameter used in ubUnder
+#' @param w parameter used in ubUnder
+#' @param verbose print extra information (TRUE/FALSE)
+#'
+#'
+#' @details The argument type can take the following values: "ubOver" (over-sampling), "ubUnder" (under-sampling), "ubSMOTE" (SMOTE), "ubOSS" (One Side Selection), "ubCNN" (Condensed Nearest Neighbor), "ubENN" (Edited Nearest Neighbor), "ubNCL" (Neighborhood Cleaning Rule), "ubTomek" (Tomek Link).
+#'
+#'
+#' @return The function returns a list: 
+#'  \item{X}{input variables}
+#'  \item{Y}{response variable}
+#'  \item{id.rm}{index of instances removed if availble in the technique selected}
+#'
+#'
+#' @seealso \code{\link{ubRacing}}, \code{\link{ubOver}}, \code{\link{ubUnder}}, \code{\link{ubSMOTE}}, \code{\link{ubOSS}}, \code{\link{ubCNN}}, \code{\link{ubENN}}, \code{\link{ubNCL}}, \code{\link{ubTomek}}
+#'
+#' @references Dal Pozzolo, Andrea, et al. "Racing for unbalanced methods selection." Intelligent Data Engineering and Automated Learning - IDEAL 2013. Springer Berlin Heidelberg, 2013. 24-31.
+#'
+#' @examples
+#' # use Racing to select the best technique for an unbalanced dataset
+#' library(unbalanced)
+#' data(ubIonosphere)
+#' 
+#' n<-ncol(ubIonosphere)
+#' output<-ubIonosphere$Class
+#' input<-ubIonosphere[ ,-n]
+#' # balance the dataset
+#' data<-ubBalance(X= input, Y=output, type="ubSMOTE", percOver=300, percUnder=150, verbose=TRUE)
+#' balancedData<-cbind(data$X,data$Y)
+#'
+#' @export
 ubBalance <-
 function(X, Y, type = "ubSMOTE", positive = 1, 
                       percOver = 200, percUnder = 200, k = 5, 
